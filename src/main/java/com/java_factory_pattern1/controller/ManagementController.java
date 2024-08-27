fe9f0a4d8cae8252bbf689bda34deede330c4045
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-08-27 12:15:47 -0600

    add the principal component into a controller

--- src/main/java/com/java_factory_pattern1/controller/ManagementController.java
+++ src/main/java/com/java_factory_pattern1/controller/ManagementController.java
@@ -1,4 +1,14 @@
 package com.java_factory_pattern1.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class ManagementController {
+
+  @GetMapping("/get")
+  void getMapping() {
+  }
 }
