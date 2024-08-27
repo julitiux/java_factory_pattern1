d997326ae4e63b733ab3fbb60c5125d5ddeb6086
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-08-27 12:51:59 -0600

    add abstract method createRequest

--- src/main/java/com/java_factory_pattern1/service/ServiceClient.java
+++ src/main/java/com/java_factory_pattern1/service/ServiceClient.java
@@ -1,4 +1,11 @@
 package com.java_factory_pattern1.service;
 
+import com.java_factory_pattern1.model.ApiRequest;
+import com.java_factory_pattern1.model.ClientRequest;
+import org.springframework.http.HttpHeaders;
+
 public abstract class ServiceClient {
+
+  public abstract ClientRequest createRequest(ApiRequest apiRequest, HttpHeaders httpHeaders);
+
 }
