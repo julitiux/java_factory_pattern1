4d99b571c24aa7d55621ddc19c210a413c1d9d3b
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-08-27 12:42:18 -0600

    add method to execute the process client

--- src/main/java/com/java_factory_pattern1/service/ManagementService.java
+++ src/main/java/com/java_factory_pattern1/service/ManagementService.java
@@ -1,7 +1,13 @@
 package com.java_factory_pattern1.service;
 
+import com.java_factory_pattern1.model.ApiRequest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ManagementService {
+
+  public void execClient(ApiRequest apiRequest, HttpHeaders httpHeaders) {
+  }
+
 }
