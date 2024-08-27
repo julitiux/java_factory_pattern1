9af81b220c67bbec18fe6ce0269acfdd1ef36e9e
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-08-27 12:52:52 -0600

    add abstract method callService

--- src/main/java/com/java_factory_pattern1/service/ServiceClient.java
+++ src/main/java/com/java_factory_pattern1/service/ServiceClient.java
@@ -2,10 +2,13 @@ package com.java_factory_pattern1.service;
 
 import com.java_factory_pattern1.model.ApiRequest;
 import com.java_factory_pattern1.model.ClientRequest;
+import com.java_factory_pattern1.model.ClientResponse;
 import org.springframework.http.HttpHeaders;
 
 public abstract class ServiceClient {
 
   public abstract ClientRequest createRequest(ApiRequest apiRequest, HttpHeaders httpHeaders);
+
+  public abstract ClientResponse callService(ClientRequest clientRequest, HttpHeaders httpHeaders);
 
 }
