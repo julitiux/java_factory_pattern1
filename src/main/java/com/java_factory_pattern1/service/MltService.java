14465c27177f9d01a945110569897b693c99b39f
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-08-28 11:14:51 -0600

    convert class into abstract and extends ServiceClient. Add method abstract methodFallback

--- src/main/java/com/java_factory_pattern1/service/MltService.java
+++ src/main/java/com/java_factory_pattern1/service/MltService.java
@@ -1,7 +1,22 @@
 package com.java_factory_pattern1.service;
 
+import com.fasterxml.jackson.databind.type.MapLikeType;
+import com.java_factory_pattern1.client.MltFeignClient;
+import com.java_factory_pattern1.model.ApiRequest;
+import com.java_factory_pattern1.model.ClientRequest;
+import com.java_factory_pattern1.model.ClientResponse;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MltService {
+public abstract class MltService extends ServiceClient {
+
+  protected final MltFeignClient mltFeignClient;
+
+  protected MltService(MltFeignClient mltFeignClient) {
+    this.mltFeignClient = mltFeignClient;
+  }
+
+  public abstract  ClientResponse methodFallback(ClientRequest clientRequest, HttpHeaders httpHeaders, Exception exception);
+
 }
