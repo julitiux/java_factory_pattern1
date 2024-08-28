23b780569a72acb6a8ac2ccc0f0f942c22e63b58
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-08-28 14:28:01 -0600

    extends class MltService into FinalService

--- src/main/java/com/java_factory_pattern1/service/impl/FinalService.java
+++ src/main/java/com/java_factory_pattern1/service/impl/FinalService.java
@@ -1,7 +1,37 @@
 package com.java_factory_pattern1.service.impl;
 
+import com.java_factory_pattern1.client.MltFeignClient;
+import com.java_factory_pattern1.model.ApiRequest;
+import com.java_factory_pattern1.model.ClientRequest;
+import com.java_factory_pattern1.model.ClientResponse;
+import com.java_factory_pattern1.service.MltService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 
 @Service
-public class FinalService {
+public class FinalService extends MltService {
+
+  protected FinalService(MltFeignClient mltFeignClient) {
+    super(mltFeignClient);
+  }
+
+  @Override
+  public ClientResponse methodFallback(ClientRequest clientRequest, HttpHeaders httpHeaders, Exception exception) {
+    return null;
+  }
+
+  @Override
+  public ClientRequest createRequest(ApiRequest apiRequest, HttpHeaders httpHeaders) {
+    return null;
+  }
+
+  @Override
+  public ClientResponse callService(ClientRequest clientRequest, HttpHeaders httpHeaders) {
+    return null;
+  }
+
+  @Override
+  public void validResponse(ClientResponse clientResponse) {
+
+  }
 }
