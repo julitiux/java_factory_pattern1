74bc460ab2a614bd28ca5ae8f99bbc781399f834
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-08-28 10:35:10 -0600

    add method getService, this method return a ServiceClient

--- src/main/java/com/java_factory_pattern1/service/ServiceClientFactory.java
+++ src/main/java/com/java_factory_pattern1/service/ServiceClientFactory.java
@@ -1,7 +1,13 @@
 package com.java_factory_pattern1.service;
 
+import com.java_factory_pattern1.model.ApiRequest;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ServiceClientFactory {
+
+  public ServiceClient getService(ApiRequest apiRequest) {
+    return null;
+  }
+
 }
