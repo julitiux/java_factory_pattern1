cf0491f0e2a4c363f9b5c71dacda018cef325101
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-08-29 23:31:33 -0600

    inject bean FinalService into ServiceClientFactory

--- src/main/java/com/java_factory_pattern1/service/ServiceClientFactory.java
+++ src/main/java/com/java_factory_pattern1/service/ServiceClientFactory.java
@@ -1,10 +1,17 @@
 package com.java_factory_pattern1.service;
 
 import com.java_factory_pattern1.model.ApiRequest;
+import com.java_factory_pattern1.service.impl.FinalService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ServiceClientFactory {
+
+  private final FinalService finalService;
+
+  public ServiceClientFactory(FinalService finalService) {
+    this.finalService = finalService;
+  }
 
   public ServiceClient getService(ApiRequest apiRequest) {
     return null;
