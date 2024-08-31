5a542d084fa27bd3371604bd8950584a1bc5bd42
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-08-30 21:37:43 -0600

    inject the bean ManagementService into ManagementController

--- src/main/java/com/java_factory_pattern1/controller/ManagementController.java
+++ src/main/java/com/java_factory_pattern1/controller/ManagementController.java
@@ -1,5 +1,8 @@
 package com.java_factory_pattern1.controller;
 
+import com.java_factory_pattern1.model.ApiRequest;
+import com.java_factory_pattern1.service.ManagementService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class ManagementController {
 
+  final private ManagementService managementService;
+
+  public ManagementController(ManagementService managementService) {
+    this.managementService = managementService;
+  }
+
   @GetMapping("/get")
-  void getMapping() {
+  void getMapping(ApiRequest apiRequest, HttpHeaders httpHeaders) {
+
+    managementService.execClient(apiRequest, httpHeaders);
+
   }
 }
