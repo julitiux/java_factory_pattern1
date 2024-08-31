6ae11234f06aa4eceb62322feba82b08fdedb77e
Julio Ramirez <rrodriguez.julio@gmail.com>
2024-08-30 21:46:54 -0600

    rename the class MltService -> AnyService because I think the name It could be more generic into a any service

--- src/main/java/com/java_factory_pattern1/service/AnyService.java
+++ src/main/java/com/java_factory_pattern1/service/AnyService.java
@@ -1,19 +1,17 @@
 package com.java_factory_pattern1.service;
 
-import com.fasterxml.jackson.databind.type.MapLikeType;
 import com.java_factory_pattern1.client.MltFeignClient;
-import com.java_factory_pattern1.model.ApiRequest;
 import com.java_factory_pattern1.model.ClientRequest;
 import com.java_factory_pattern1.model.ClientResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 
 @Service
-public abstract class MltService extends ServiceClient {
+public abstract class AnyService extends ServiceClient {
 
   protected final MltFeignClient mltFeignClient;
 
-  protected MltService(MltFeignClient mltFeignClient) {
+  protected AnyService(MltFeignClient mltFeignClient) {
     this.mltFeignClient = mltFeignClient;
   }
 
--- src/main/java/com/java_factory_pattern1/service/impl/FinalService.java
+++ src/main/java/com/java_factory_pattern1/service/impl/FinalService.java
@@ -4,12 +4,12 @@ import com.java_factory_pattern1.client.MltFeignClient;
 import com.java_factory_pattern1.model.ApiRequest;
 import com.java_factory_pattern1.model.ClientRequest;
 import com.java_factory_pattern1.model.ClientResponse;
-import com.java_factory_pattern1.service.MltService;
+import com.java_factory_pattern1.service.AnyService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 
 @Service
-public class FinalService extends MltService {
+public class FinalService extends AnyService {
 
   protected FinalService(MltFeignClient mltFeignClient) {
     super(mltFeignClient);
